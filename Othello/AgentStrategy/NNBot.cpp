c6d4b972352b607b3f8adf29c9dffdcb3e741d44
AriNoa <ikasumi.nekoneko@gmail.com>
2020-11-08 17:57:44 +0900

    Bot修正

--- Othello/AgentStrategy/NNBot.cpp
+++ Othello/AgentStrategy/NNBot.cpp
@@ -6,18 +6,18 @@ NNBot::NNBot(const NeuralNetwork& nn) {
 }
 
 
-std::pair<double, double> NNBot::getInput(const otl::Team& team) const {
+std::pair<double, double> NNBot::getInput(const otl::Team& myTeam, const otl::Team& team) const {
 	if (team == otl::Team::None) return { 0, 0 };
-	if (team == _myTeam) return { 0, 1 };
+	if (team == myTeam) return { 0, 1 };
 	return { 1, 0 };
 }
 
-std::vector<double> NNBot::getInputs(const std::vector<std::vector<otl::Team>>& board) const {
+std::vector<double> NNBot::getInputs(const otl::Team& myTeam, const std::vector<std::vector<otl::Team>>& board) const {
 	std::vector<double> inputs;
 
 	for (int y = 0; y < board.size(); y++) {
 		for (int x = 0; x < board[y].size(); x++) {
-			const auto input = getInput(board[y][x]);
+			const auto input = getInput(myTeam, board[y][x]);
 
 			inputs.push_back(input.first);
 			inputs.push_back(input.second);
@@ -32,9 +32,9 @@ otl::Point NNBot::answer(const otl::Othello& othello) {
 	using namespace std;
 	using namespace otl;
 
-	const auto board = othello.getBoard();
+	const Team myTeam = othello.getActiveTeam();
 
-	double max = 0;
+	double max = -1;
 	Point maxPos;
 
 	for (int y = 0; y < Othello::height; y++) {
@@ -44,9 +44,8 @@ otl::Point NNBot::answer(const otl::Othello& othello) {
 			if (!othello.canPutStone(pos)) continue;
 
 			Othello copyOthello = othello;
-			copyOthello.putStone(pos);
 
-			const auto inputs = getInputs(copyOthello.getBoard());
+			const auto inputs = getInputs(myTeam, copyOthello.getBoard());
 			const double output = _nn.forward(inputs).front();
 
 			if (output > max) {
--- Othello/AgentStrategy/NNBot.h
+++ Othello/AgentStrategy/NNBot.h
@@ -5,14 +5,13 @@
 
 class NNBot : public IAgentStrategy {
 private:
-	otl::Team _myTeam;
 	NeuralNetwork _nn;
 
 public:
 	NNBot(const NeuralNetwork& nn);
 
-	std::pair<double, double> getInput(const otl::Team& team) const;
-	std::vector<double> getInputs(const std::vector<std::vector<otl::Team>>& board) const;
+	std::pair<double, double> getInput(const otl::Team& myTeam, const otl::Team& team) const;
+	std::vector<double> getInputs(const otl::Team& myTeam, const std::vector<std::vector<otl::Team>>& board) const;
 
 	virtual std::string getName() const { return "NNBot"; }
 	virtual otl::Point answer(const otl::Othello& othello) override;
