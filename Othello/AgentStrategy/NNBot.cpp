ae6acea3bcb8b65ffaceb02ef40d7f1378843f6a
AriNoa <ikasumi.nekoneko@gmail.com>
2020-11-23 23:41:56 +0900

    特徴量を変更

--- Othello/AgentStrategy/NNBot.cpp
+++ Othello/AgentStrategy/NNBot.cpp
@@ -1,38 +1,175 @@
 #include "NNBot.h"
 
 
-NNBot::NNBot(const NeuralNetwork& nn) {
-	_nn = nn;
+nne::LineEvaluation::LineEvaluation(
+	const NeuralNetwork& nn,
+	const otl::Team& myTeam,
+	const std::vector<std::vector<otl::Team>> board
+)	: _nn(nn)
+	, _myTeam(myTeam)
+	, _board(board) {
 }
 
-
-std::pair<double, double> NNBot::getInput(const otl::Team& myTeam, const otl::Team& team) const {
+std::pair<double, double> nne::LineEvaluation::getInput(const otl::Team& team) const {
 	if (team == otl::Team::None) return { 0, 0 };
-	if (team == myTeam) return { 0, 1 };
+	if (team == _myTeam) return { 0, 1 };
 	return { 1, 0 };
 }
 
-std::vector<double> NNBot::getInputs(const otl::Team& myTeam, const std::vector<std::vector<otl::Team>>& board) const {
+std::vector<double> nne::LineEvaluation::getInputs(const Line& line) const {
 	std::vector<double> inputs;
 
-	for (int y = 0; y < board.size(); y++) {
-		for (int x = 0; x < board[y].size(); x++) {
-			const auto input = getInput(myTeam, board[y][x]);
+	for (size_t i = 0; i<line.length; i++) {
+		const otl::Point pos = line.corner + line.dir * (int)i;
 
-			inputs.push_back(input.first);
-			inputs.push_back(input.second);
-		}
+		const auto input = getInput(_board[pos.y][pos.x]);
+
+		inputs.push_back(input.first);
+		inputs.push_back(input.second);
 	}
 
 	return inputs;
 }
 
+double nne::LineEvaluation::evaluate(const Line& line) const {
+	const auto inputs = getInputs(line);
+
+	return _nn.forward(inputs).front();
+}
+
+
+std::vector<nne::Line> nne::BoardEvaluation::initLines(const size_t& length) {
+	using namespace std;
+	using namespace otl;
+
+	std::vector<nne::Line> lines;
+
+	const size_t XB = 0;
+	const size_t YB = 0;
+	const size_t XE = Othello::width-1;
+	const size_t YE = Othello::height-1;
+
+	const vector<pair<Point, DirVector>> vecs = {
+		{{XB, YB}, { 1,  1}},
+		{{XB, YE}, { 1, -1}},
+		{{XE, YB}, {-1,  1}},
+		{{XE, YE}, {-1, -1}},
+	};
+
+	for (const auto& vec : vecs) {
+		const otl::Point& corner = vec.first;
+		const int& dx = vec.second.x;
+		const int& dy = vec.second.y;
+		lines.push_back({ corner, {dx, dy}, length });
+		lines.push_back({ corner, { 0, dy}, length });
+		lines.push_back({ corner, {dx,  0}, length });
+	}
+
+	return lines;
+}
+
+nne::BoardEvaluation::Board<std::vector<size_t>> nne::BoardEvaluation::initLineIndexBoard(
+	const std::vector<nne::Line>& lines
+) {
+	using namespace std;
+	using namespace otl;
+
+	Board<vector<size_t>> indexBoard = Board<vector<size_t>>(
+		Othello::height, vector<vector<size_t>>(Othello::width)
+	);
+
+	for (size_t index = 0; index < lines.size(); index++) {
+		const Line& line = lines[index];
+		const Point& corner = line.corner;
+		const DirVector& dir = line.dir;
+
+		for (size_t i = 0; i < line.length; i++) {
+			const Point& pos = corner + dir * (int)i;
+
+			indexBoard[pos.y][pos.x].push_back(index);
+		}
+	}
+
+	return indexBoard;
+}
+
+nne::BoardEvaluation::BoardEvaluation(
+	const std::vector<double>& defaultLineEvals, 
+	const NeuralNetwork& nn,
+	const otl::Othello& othello
+)	: _defaultLineEvals(defaultLineEvals)
+	, _nn(nn)
+	, _othello(othello) 
+	, _defaultEvalSum(calculateEvalSum(_defaultLineEvals)){
+	_evalSum = _defaultEvalSum;
+	_linesIgnore = std::vector<bool>(lines.size(), false);
+}
+
+void nne::BoardEvaluation::pointEval(const otl::Point& pos, const LineEvaluation& lineEval) {
+	for (const auto& index : lineIndexBoard[pos.y][pos.x]) {
+		if (_linesIgnore[index]) continue;
+
+		const Line& line = lines[index];
+		const double eval = lineEval.evaluate(line);
+		_evalSum += eval - _defaultLineEvals[index];
+
+		_linesIgnore[index] = true;
+	}
+}
+
+double nne::BoardEvaluation::evaluate(const otl::Point& putPos) {
+	using namespace std;
+	using namespace otl;
+
+	_evalSum = _defaultEvalSum;
+	_linesIgnore = std::vector<bool>(lines.size(), false);
+
+	const vector<DirVector> dirVecs = createDirVectors();
+	const vector<vector<Team>>& board = _othello.getBoard();
+	const Team& myTeam = _othello.getActiveTeam();
+
+	Othello copyOthello = _othello;
+	copyOthello.putStone(putPos);
+	const vector<vector<Team>>& nextBoard = copyOthello.getBoard();
+
+	const LineEvaluation lineEval(_nn, myTeam, nextBoard);
+
+	pointEval(putPos, lineEval);
+
+	for (const auto& dir : dirVecs) {
+		if (!_othello.canTurnOverLine(putPos, dir)) continue;
+
+		size_t i = 0;
+		while (true) {
+			i++;
+			Point pos = putPos + dir * (int)i;
+			if (board[pos.y][pos.x] == myTeam) break;
+			pointEval(pos, lineEval);
+		}
+	}
+
+	return _evalSum;
+}
+
+
+NNBot::NNBot(const NeuralNetwork& nn) {
+	_nn = nn;
+}
 
 otl::Point NNBot::answer(const otl::Othello& othello) {
 	using namespace std;
 	using namespace otl;
+	using namespace nne;
 
 	const Team myTeam = othello.getActiveTeam();
+
+	vector<double> defaultLineEvals;
+	LineEvaluation lineEval(_nn, myTeam, othello.getBoard());
+	for (const auto& line : BoardEvaluation::lines) {
+		defaultLineEvals.push_back(lineEval.evaluate(line));
+	}
+
+	BoardEvaluation boardEval(defaultLineEvals, _nn, othello);
 
 	double max = -1;
 	Point maxPos;
@@ -43,14 +180,10 @@ otl::Point NNBot::answer(const otl::Othello& othello) {
 
 			if (!othello.canPutStone(pos)) continue;
 
-			Othello copyOthello = othello;
+			const double eval = boardEval.evaluate(pos);
 
-			copyOthello.putStone(pos);
-			const auto inputs = getInputs(myTeam, copyOthello.getBoard());
-			const double output = _nn.forward(inputs).front();
-
-			if (output > max) {
-				max = output;
+			if (eval > max) {
+				max = eval;
 				maxPos = pos;
 			}
 		}
--- Othello/AgentStrategy/NNBot.h
+++ Othello/AgentStrategy/NNBot.h
@@ -3,15 +3,77 @@
 #include "NN/NeuralNetwork.h"
 
 
+namespace nne {
+	struct Line {
+		otl::Point corner;
+		otl::DirVector dir;
+		size_t length;
+	};
+
+	class LineEvaluation {
+	private:
+		const NeuralNetwork& _nn;
+		const otl::Team& _myTeam;
+		const std::vector<std::vector<otl::Team>> _board;
+
+	public:
+		LineEvaluation(
+			const NeuralNetwork& nn,
+			const otl::Team& myTeam,
+			const std::vector<std::vector<otl::Team>> board
+		);
+
+		std::pair<double, double> getInput(const otl::Team& team) const;
+		std::vector<double> getInputs(const Line& line) const;
+
+		double evaluate(const Line& line) const;
+	};
+
+	class BoardEvaluation {
+	public:
+		template<typename T>
+		using Board = std::vector<std::vector<T>>;
+
+		static std::vector<nne::Line> initLines(const size_t& length);
+		static Board<std::vector<size_t>> initLineIndexBoard(const std::vector<nne::Line>& lines);
+		static double calculateEvalSum(const std::vector<double>& evals) {
+			double sum = 0;
+			for (const auto& eval : evals) { sum += eval; }
+			return sum;
+		}
+
+		static constexpr size_t lineLength = 3;
+		static inline const std::vector<nne::Line> lines = initLines(lineLength);
+		static inline const Board<std::vector<size_t>> lineIndexBoard = initLineIndexBoard(lines);
+
+	private:
+		const std::vector<double>& _defaultLineEvals;
+		const NeuralNetwork& _nn;
+		const otl::Othello& _othello;
+		const double _defaultEvalSum;
+
+		double _evalSum;
+		std::vector<bool> _linesIgnore;
+
+	public:
+		BoardEvaluation(
+			const std::vector<double>& defaultLineEvals,
+			const NeuralNetwork& nn,
+			const otl::Othello& othello
+		);
+
+		void pointEval(const otl::Point& pos, const LineEvaluation& lineEval);
+
+		double evaluate(const otl::Point& putPos);
+	};
+}
+
 class NNBot : public IAgentStrategy {
 private:
 	NeuralNetwork _nn;
 
 public:
 	NNBot(const NeuralNetwork& nn);
-
-	std::pair<double, double> getInput(const otl::Team& myTeam, const otl::Team& team) const;
-	std::vector<double> getInputs(const otl::Team& myTeam, const std::vector<std::vector<otl::Team>>& board) const;
 
 	virtual std::string getName() const { return "NNBot"; }
 	virtual otl::Point answer(const otl::Othello& othello) override;
